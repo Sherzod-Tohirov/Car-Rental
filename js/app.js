f886c4f4c4e77944e26c1778d72d78a9f41d1a6e
Sherzod-Tohirov <sherzodtohirov862@gmail.com>
2023-09-16 21:55:13 +0500

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,266 @@
+// Close and Menu 
+
+const elMenuBtn = get('js-menu-btn');
+const elCloseBtn = get('js-close-btn');
+const elInnerHeader = get('js-inner-header');
+const elOverlay = get('js-overlay');
+let switched = false;
+
+
+elMenuBtn.addEventListener('click', (evt) => {
+    switched = true;
+    elOverlay.style.display = 'block';    
+    setTimeout(() => {
+        elOverlay.classList.add('move');
+    }, 0);
+    setTimeout(() => {
+        elInnerHeader.style.display = 'flex';
+    }, 100);
+    setTimeout(() => {
+        elInnerHeader.classList.add('move'); 
+    }, 150);
+});
+
+
+elCloseBtn.addEventListener('click', (evt) => {
+    switched = false;
+    elInnerHeader.classList.remove('move');
+    elOverlay.classList.remove('move');
+    setTimeout(() => {
+        elInnerHeader.style.display = 'none';
+        elOverlay.style.display = 'none';   
+    }, 300); 
+});
+
+
+
+window.addEventListener('resize', (evt) => { 
+
+   if(window.matchMedia('(min-width: 1045px)').matches) {
+    elInnerHeader.style.display = 'flex';    
+    elOverlay.style.display = 'none'; 
+   }
+
+   if(!window.matchMedia('(min-width: 1045px)').matches && switched) {
+        elOverlay.style.display = 'block'; 
+   }
+
+   if(!window.matchMedia('(min-width: 1045px)').matches && !switched) {
+        elInnerHeader.style.display = 'none';    
+        elOverlay.style.display = 'none'; 
+   }
+
+});
+
+
+// Slider controllers 
+
+const elPrevBtn = get('slider-prev-btn');
+const elNextBtn = get('slider-next-btn');
+const elSliderWrapper = get('hero__slider-wrapper');
+const slides = Array.from(elSliderWrapper.children);
+const slideHeight = slides[0].clientHeight + 35;
+let height = elSliderWrapper.scrollHeight;
+let currentSlide;
+let currentSlideIndex;
+const elSliderViewWrapper = get('hero__slider-view');
+const views = Array.from(elSliderViewWrapper.children);
+elNextBtn.addEventListener('click', (evt) => {
+
+    currentSlide = slides.find(slide => {
+        return slide.classList.contains('active-slide');
+    });
+    currentSlideIndex = slides.findIndex(slide => {
+        return slide.classList.contains('active-slide');
+    });
+
+    currentSlideIndex += 1;
+
+    if(currentSlideIndex == slides.length - 1) {
+    elSliderWrapper.scrollTo(0, height);
+    }else {
+    elSliderWrapper.scrollTo(0, currentSlideIndex * slideHeight);
+    }
+
+    if(currentSlide.nextElementSibling) {
+        currentSlide.classList.remove('active-slide');
+        currentSlide.nextElementSibling.classList.add('active-slide');
+    }
+
+    changeItemView();
+
+
+});
+
+elPrevBtn.addEventListener('click', (evt) => {
+
+    currentSlide = slides.find(slide => {
+        return slide.classList.contains('active-slide');
+    });
+    currentSlideIndex = slides.findIndex(slide => {
+        return slide.classList.contains('active-slide');
+    });
+
+
+    if(currentSlide.previousElementSibling) {
+        elSliderWrapper.scrollTo(elSliderWrapper.scrollTop, elSliderWrapper.scrollTop - slideHeight);
+        currentSlide.classList.remove('active-slide');
+        currentSlide.previousElementSibling.classList.add('active-slide');
+    }
+
+    changeItemView();
+
+
+});
+
+// Hero Slider view items 
+
+elSliderViewWrapper.addEventListener('click', (evt) => {
+    if(evt.target.localName === 'span') {
+        const viewIndex = views.findIndex(item => item === evt.target);
+        views.forEach(item => {
+            item.classList.remove('active-slider-item');
+        }); 
+
+        evt.target.classList.add('active-slider-item');
+        
+        elSliderWrapper.scrollTo(0, slideHeight * viewIndex);
+        
+    }
+})
+
+function changeItemView() {
+    
+    if(!isNaN(currentSlideIndex)) {
+        let currentSlideInd = slides.findIndex(item => {
+        return item.classList.contains('active-slide');
+        });
+        console.log(currentSlideInd);
+        views.forEach((item, index) => {
+            if(index != currentSlideInd) {
+            item.classList.remove('active-slider-item');
+            }else {
+            if(!item.classList.contains('active-slider-item')) {
+                item.classList.add('active-slider-item');
+            }
+            }
+        });
+      
+}
+
+
+}
+
+
+// View Products 
+
+const fragment = new DocumentFragment();
+const elList = get('js-product-list');
+const elListTemp = get('js-product-temp');
+const elBtns = Array.from(get('js-btn-wrapper').children);
+const elCompactBtn = get('js-compact-btn');
+const elSportCarBtn = get('js-sport-car-btn');
+const elVansBtn = get('js-vans-btn');
+const elAllBtn = get('js-all-btn');
+
+
+function renderProducts(data, node, type) {
+   
+    if(type === 'compact') {
+        data = data.filter(item => item.group === 'compact');
+    }
+
+    if(type === 'sports') {
+        data = data.filter(item => item.group === 'sports');
+    }
+
+    if(type === 'vans') {
+        data = data.filter(item => item.group === 'vans');
+    }
+
+    node.innerHTML = '';
+    data.forEach(item => {
+        const temp = elListTemp.content.cloneNode(true);
+
+        temp.querySelector('.products__item-title').title = item.title;
+        temp.querySelector('.products__item-title').textContent = item.title;
+
+        temp.querySelector('.products__item-desc').title = item.type;
+        temp.querySelector('.products__item-desc').textContent = item.type;
+
+        temp.querySelector('.products__img').src = item.image[0];
+        temp.querySelector('.products__img').srcset = `${item.image[0]} 1x, ${item.image[1]} 2x`;
+        temp.querySelector('.products__img').alt = item.title;
+        
+        temp.querySelector('.js-seats').textContent = `${item.seats} Seats`;
+        temp.querySelector('.js-transmission').textContent = item.transmission;
+        temp.querySelector('.js-years').textContent = `${item.years}+ Years`;
+        temp.querySelector('.js-consumption').textContent = item.consumption;
+        
+        fragment.appendChild(temp);
+
+    });
+
+    node.appendChild(fragment);
+}
+
+function printsProducts(btn, type, isAll = false) {
+    btn.addEventListener('click', (evt) => {
+        renderProducts(cars, elList, `${type}`);
+        
+        if(isAll) {
+            elBtns.forEach(btn => btn.classList.remove('active-btn'));
+            btn.style.display = 'none';
+        }
+    });   
+    
+   
+}
+
+function get(className, getAll = false) {
+    if(getAll) {
+      return document.querySelectorAll(`.${className}`);
+    }
+    return document.querySelector(`.${className}`);
+}
+
+
+
+// Default render compact 
+
+renderProducts(cars, elList, 'compact');
+
+// Btn color changer
+
+elBtns.forEach(btn => {
+   btn.addEventListener('click', (evt) => {
+    elBtns.forEach(btn => btn.classList.remove('active-btn'));
+      if(!btn.classList.contains('.active-btn')) {
+           btn.classList.add('active-btn');
+       }
+    //    View all btn visible
+
+       if(elAllBtn.style.display == 'none') {
+          elAllBtn.style.display = 'block';
+        }
+   }); 
+    
+   
+});
+
+// View other types 
+
+printsProducts(elCompactBtn, 'compact');
+printsProducts(elSportCarBtn, 'sports');
+printsProducts(elVansBtn, 'vans');
+printsProducts(elAllBtn, 'all', true);
+
+
+
+
+
+
+
+
+
+
---
+++ js/data.js
@@ -0,0 +1,182 @@
+cars = [
+
+	{
+		title: "Ford Fiesta",
+		type: "Economy Car",
+		image: ["./images/png/fiesta.png", "./images/png/fiesta@2x.png"],
+		seats: "5",
+		transmission: "Automatic",
+		years: "21",
+		consumption: "1-lit / 2.5 km",
+		group: "compact"
+	},
+
+	{
+		title: "Nissan Versa",
+		type: "Compact Car",
+		image: ["./images/png/versa.png", "./images/png/versa@2x.png"],
+		seats: "5",
+		transmission: "Automatic",
+		years: "21",
+		consumption: "1-lit / 2.5 km",
+		group: "compact"
+	},
+
+	{
+		title: "Toyota Corolla",
+		type: "Mid-size Car",
+		image: ["./images/png/corolla.png", "./images/png/corolla@2x.png"],
+		seats: "5",
+		transmission: "Automatic",
+		years: "21",
+		consumption: "1-lit / 2.5 km",
+		group: "compact"
+	},
+
+	{
+		title: "Nissan Rogue",
+		type: "mid-size SUV",
+		image: ["./images/png/rogue.png", "./images/png/rogue@2x.png"],
+		seats: "5",
+		transmission: "Automatic",
+		years: "21",
+		consumption: "1-lit / 2.5 km",
+		group: "compact"
+	},
+
+	{
+		title: "Chevy Traverse",
+		type: "Full-size SUV",
+		image: ["./images/png/traverse.png", "./images/png/traverse@2x.png"],
+		seats: "5",
+		transmission: "Automatic",
+		years: "21",
+		consumption: "1-lit / 2.5 km",
+		group: "compact"
+	},
+
+	{
+		title: "Nissan Altima",
+		type: "Full-size Car",
+		image: ["./images/png/altima.png", "./images/png/altima@2x.png"],
+		seats: "5",
+		transmission: "Automatic",
+		years: "21",
+		consumption: "1-lit / 2.5 km",
+		group: "compact"
+	},
+
+
+	{
+		title: "Royce rolls ghost",
+		type: "Full-size Car",
+		image: ["./images/png/roycerolls.png", "./images/png/roycerolls@2x.png"],
+		seats: "5",
+		transmission: "Automatic",
+		years: "21",
+		consumption: "1-lit / 2.5 km",
+		group: "sports"
+	},
+
+
+	{
+		title: "Mercedes s class",
+		type: "Full-size Car",
+		image: ["./images/png/sclass.png", "./images/png/sclass@2x.png"],
+		seats: "5",
+		transmission: "Automatic",
+		years: "21",
+		consumption: "1-lit / 2.5 km",
+		group: "sports"
+	},
+
+	{
+		title: "Lamborgini urus",
+		type: "Mid-size Car",
+		image: ["./images/png/urus.png", "./images/png/urus@2x.png"],
+		seats: "5",
+		transmission: "Automatic",
+		years: "21",
+		consumption: "1-lit / 2.5 km",
+		group: "sports"
+	},
+
+	{
+		title: "Mercedes g63 amg",
+		type: "Mid-size SUV",
+		image: ["./images/png/g63.png", "./images/png/g63@2x.png"],
+		seats: "5",
+		transmission: "Automatic",
+		years: "21",
+		consumption: "1-lit / 2.5 km",
+		group: "sports"
+	},
+
+
+	{
+		title: "15-Passenger Ford Transit",
+		type: "Full-size Car",
+		image: ["./images/png/ford_transit.png", "./images/png/ford_transit@2x.png"],
+		seats: "5",
+		transmission: "Automatic",
+		years: "21",
+		consumption: "1-lit / 2.5 km",
+		group: "vans"
+	},
+
+	{
+		title: "Chrysler Pacifica",
+		type: "Full-size Car",
+		image: ["./images/png/pacifica.png", "./images/png/pacifica@2x.png"],
+		seats: "5",
+		transmission: "Automatic",
+		years: "21",
+		consumption: "1-lit / 2.5 km",
+		group: "vans"
+	},
+
+	{
+		title: "Chevy Silverado 4500HD",
+		type: "Mid-size Car",
+		image: ["./images/png/chevy_silverado.png", "./images/png/chevy_silverado@2x.png"],
+		seats: "5",
+		transmission: "Automatic",
+		years: "21",
+		consumption: "1-lit / 2.5 km",
+		group: "vans"
+	},
+
+	{
+		title: "12-Passenger Ford Transit",
+		type: "Mid-size SUV",
+		image: ["./images/png/ford_transit_mid.png", "./images/png/ford_transit_mid@2x.png"],
+		seats: "5",
+		transmission: "Automatic",
+		years: "21",
+		consumption: "1-lit / 2.5 km",
+		group: "vans"
+	},
+
+	{
+		title: "15-Passenger Ford Transit",
+		type: "Mid-size SUV",
+		image: ["./images/png/ford_transit_suv.png", "./images/png/ford_transit_suv@2x.png"],
+		seats: "5",
+		transmission: "Automatic",
+		years: "21",
+		consumption: "1-lit / 2.5 km",
+		group: "vans"
+	},
+
+	{
+		title: "Mercedes-Benz Sprinter",
+		type: "Mid-size SUV",
+		image: ["./images/png/sprinter.png", "./images/png/sprinter@2x.png"],
+		seats: "5",
+		transmission: "Automatic",
+		years: "21",
+		consumption: "1-lit / 2.5 km",
+		group: "vans"
+	},
+
+];
